20cd73f19b9247f86be43b9e077c2f7eacfb8910
Khagendra1814138 <75677824+Khagendra1814138@users.noreply.github.com>
2023-09-21 14:04:33 +0100

    asd

--- src/Components/NavigationBar/NavigationBar.js
+++ src/Components/NavigationBar/NavigationBar.js
@@ -3,13 +3,17 @@ import { Link } from "react-router-dom";
 import "./NavigationBar.css";
 
 import { NavigationLinksArray } from "./navigationArray";
+import SilamSakma from "../../Images/silamsakmaLogo.png";
 
 function NavigationBar() {
    return (
         <div className="NavagationBar">
-            {NavigationLinksArray.map((item) => (
-                <Link className="links" to={item.link}> {item.title} </Link>
-            ))}
+            <logo className="homeLogo" style={{backgroundImage: `url(${SilamSakma})`}}></logo>
+            <ul className="linksList">
+                {NavigationLinksArray.map((item) => (
+                    <Link className="links" to={item.link}> {item.title} </Link>
+                ))}
+            </ul>
         </div>
     );
 };
