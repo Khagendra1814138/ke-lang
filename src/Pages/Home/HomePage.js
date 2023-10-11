2c7c2261b071dff049b9ad47788ee06161be4fd8
Khagendra1814138 <75677824+Khagendra1814138@users.noreply.github.com>
2023-10-11 11:43:36 +0100

    Update HomePage.js

--- src/Pages/Home/HomePage.js
+++ src/Pages/Home/HomePage.js
@@ -6,7 +6,6 @@ import ParallaxMouse from "../Paralax/ParallaxMouse";
 
 //Images Imports
 import Background2 from "../../Images/kelangBackground2.jpg";
-import rythem from "../../Images/homeImages/rythem.png";
 import complexFoot from "../../Images/homeImages/foot.png";
 import flexFoot from "../../Images/homeImages/flexFoot.png";
 import wild from "../../Images/homeImages/wild.png";
@@ -30,7 +29,7 @@ export const HomePage = () => {
             <section className="splitSection">
                 <div className="flex">
                     <h3>The <span>only music</span> is the <span>rhythmic beating</span> of the Chyabrung.</h3>
-                    <image style={{backgroundImage: `url(${rythem})`}}></image>
+                    <image style={{backgroundImage: `url(${complexFoot})`}}></image>
                 </div>
 
                 <div className="flex">
