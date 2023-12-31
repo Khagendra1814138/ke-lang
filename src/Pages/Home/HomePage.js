7078308cc9b33d301b0c1879d243c50aad354d85
Khagendra1814138 <75677824+Khagendra1814138@users.noreply.github.com>
2023-12-31 23:32:51 +0000

    small change

--- src/Pages/Home/HomePage.js
+++ src/Pages/Home/HomePage.js
@@ -2,7 +2,8 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useInView } from 'react-intersection-observer';
 import "../../Styles/HomePage.css";
-import "../../Styles/flexWrappers.css"
+import "../../Styles/wrappers&colors.css";
+import "../../Styles/videoSection.css"
 
 import ParallaxMouse from "../Paralax/ParallaxMouse";
 
@@ -27,6 +28,12 @@ import chyabrungLarge from "../../Images/homeImages/chyabrungLarge.jpg";
 
 export const HomePage = () => {
     const navigate = useNavigate();
+
+    const { ref: introImg, inView: introImgIsVisible} = useInView({
+        /* Optional options */
+        threshold: 0.5,
+        rootMargin: "50px",
+    });
 
     const { ref: aboutRef, inView: aboutSectionIsVisible} = useInView({
         /* Optional options */
@@ -53,7 +60,7 @@ export const HomePage = () => {
                     </h3>
                 </div>
 
-                <div ref={aboutRef} className={`${'largeImage'} ${aboutSectionIsVisible ? "moveUp" : "largeImage"}`} style={{backgroundImage: `url(${Background2})`}}></div>
+                <div ref={introImg} className={`${'largeImage'} ${introImgIsVisible ? "moveUp" : "largeImage"}`} style={{backgroundImage: `url(${Background2})`}}></div>
                 <div className="informationWrapper">
                     <h3>
                         The dance is mainly performed in a circular formation. Typically there are between 8 to 12 or more dancers in a kelang group. Each male dancers have a female dancer partner and stands on the left side of their male partner.
@@ -65,7 +72,7 @@ export const HomePage = () => {
                         <image style={{backgroundImage: `url(${Boys})`}} alt="boys"></image>
                         <div className="informationWrapper">
                             <h2><span>MALE </span> DANCERS</h2>
-                            <line className="purpleLine"></line>
+                            <line className="pimpPurple"></line>
                             <p>                       
                                 The Chyabrung drum is carried and played by the male dancers. Creating rhythmic beat, the dancers synchronize their foot movements with the chyabrung beat and dance in a circle formation.
                             </p>
@@ -76,7 +83,7 @@ export const HomePage = () => {
                         <image style={{backgroundImage: `url(${Girls})`}}></image>
                         <div className="informationWrapper">
                             <h2><span>FEMALE </span>DANCERS</h2>
-                            <line className="purpleLine"></line>
+                            <line className="pimpPurple"></line>
                             <p>
                                 Synchronizing their footsteps together, while gently and elegantly performing smooth hands movement, the female dancers joins the male dancers. Typically, the girls are on the left/behind of the boys dancers.
                             </p>
@@ -87,21 +94,25 @@ export const HomePage = () => {
             </section>
 
             <div className="videoWrapper">
-                <div className="videoCircle">
+                <div className="videoCircleWrapper">
+                    <div className="videoCircleTop purpleRoundedColor"></div>
+                </div>
                     <div className="videoFrame">
                         <div className="videoInformation">
                             <h2 className="">KELANG VIDEO</h2>
-                            <line className="purpleLine"></line>
+                            <line className="pimpPurple"></line>
                             <p>
                                 The video shows how the Jhyampta is played together with the Tambourine as a percussion instruments alongside the chyabrung beat.
                             </p>
                         </div>
                         <div className="video">video</div>
                     </div>
+                <div className="videoCircleWrapper">
+                    <div className="videoCircleBottom purpleRoundedColor"></div>
                 </div>
             </div>
 
-            <section className="splitSection">
+            <section className="splitSection lightTealGreen">
                 <h1>MUSIC AND DANCE STEPS</h1>
                 <div className="informationWrapper">
                     <h3>
@@ -111,22 +122,22 @@ export const HomePage = () => {
 
                 <div className="flex">
                     <h3>The <span>only music</span> is the <span>rhythmic beating</span> of the Chyabrung.</h3>
-                    <image style={{backgroundImage: `url(${ChyabrungRythem})`}}></image>
+                    <image className="darkTealGreen" style={{backgroundImage: `url(${ChyabrungRythem})`}}></image>
                 </div>
 
                 <div className="flex">
                     <h3>Dancers execute <span>synchronized</span> and <span>complicated</span> foot work.</h3>
-                    <image style={{backgroundImage: `url(${complexFoot})`}}></image>
+                    <image className="darkTealGreen" style={{backgroundImage: `url(${complexFoot})`}}></image>
                 </div>
 
                 <div className="flex">
                     <h3>...depicting graceful <span>movements</span> of <span>wild animals.</span></h3>
-                    <image style={{backgroundImage: `url(${wild})`}}></image>
+                    <image className="darkTealGreen" style={{backgroundImage: `url(${wild})`}}></image>
                 </div>
 
                 <div className="flex">
                     <h3>And countless <span>varieties</span> of flexible and energetic <span>foot movements.</span></h3>
-                    <image style={{backgroundImage: `url(${flexFoot})`}}></image>
+                    <image className="darkTealGreen" style={{backgroundImage: `url(${flexFoot})`}}></image>
                 </div>
 
                 <div className="informationWrapper">
@@ -140,7 +151,7 @@ export const HomePage = () => {
 
 
 
-            <section className="sectionSplitY">
+            <section className="sectionSplitY lightSteelBlue">
                 <h1>ABOUT THE CHYABRUNG</h1>
                 <div className="informationWrapper">
                     <h3>
@@ -158,22 +169,21 @@ export const HomePage = () => {
 
                 <div className="aboutChyabrungInfoWrapper">
                     <div className="flexColumnn">
-                        <image style={{backgroundImage: `url(${strung})`}}></image>
+                        <image className="imageFloatLeft darkSteelBlue" style={{backgroundImage: `url(${strung})`}}></image>
                         <div className="informationWrapper">
                             <h2>HOW IT'S <span>HELD</span></h2>
-                            <line className="purpleLine"></line>
+                            <line className="mediumSteelBlue"></line>
                             <p>                       
-                                The Chybarung drum is strung around the neck of the male dancers with a cord at stomach heighted.
+                                It is strung around the neck and over the left shoulder or just over the left shoulder with a cord and a strap at the stomach height.
                             </p>
                         </div>
                     </div>
                
-
                     <div className="flexColumnn">
-                        <image style={{backgroundImage: `url(${playedHand})`}}></image>
+                        <image className="imageFloatRight darkSteelBlue" style={{backgroundImage: `url(${playedHand})`}}></image>
                         <div className="informationWrapper">
                             <h2>HOW IT'S <span>PLAYED</span></h2>
-                            <line className="purpleLine"></line>
+                            <line className="mediumSteelBlue"></line>
                             <p>                       
                                 The Chyabrung is played using both hands, primarily the right palm of the hand to hit the sharp skin "Chyang" and left hand to hit the base skin "Dung". 
                             </p>
@@ -184,31 +194,8 @@ export const HomePage = () => {
                 <button onClick={() => navigate("/HowItsPlayed")}>Show More</button>
             </section>
 
-
-
-            <section className="secOverflowHidden">
-                <image className="purpleBg" style={{backgroundImage: `url(${Jhympta})`}}>
-                </image>
-                <p className="purpleBgParahraph"> 
-                    <h2><span>OTHER</span> INSTRUMENTS</h2>
-                    Chyabrung is not only the instrument played.
-                </p>
-                <button className="madeButton" onClick={() => navigate("/Instruments")}>Show More</button>
-            </section>
-            
-            <section className="secOverflowHidden">
-                <image className="purpleBg" style={{backgroundImage: `url(${chyabrungMade})`}}></image>
-                <p className="purpleBgParahraph">
-                    <h2>HOW IT'S <span>MADE</span></h2>
-                    Chyabrung is made of wood and different types of leather
-                </p>
-                <button className="madeButton" onClick={() => navigate("/HowItsMade")}>Show More</button>
-            </section>
-
-
             <div className="sectionContainer">
-                <sectioncurvedtop className="curvedTop brownRoundColor">
-               
+                <sectioncurvedtop className="curvedTop">
                     <div className="whitebox">
                         <image style={{backgroundImage: `url(${boysClothes})`}}></image>
                         <div className="informationWrapper">
@@ -219,12 +206,11 @@ export const HomePage = () => {
                             </p>
                         </div>
                     </div>
-                
                 </sectioncurvedtop>
 
                 <button className="dressButton" onClick={() => navigate("/Dresses")}>Show More</button>
 
-                <sectioncurvedbottom className="curvedBottom brownRoundColor">
+                <sectioncurvedbottom className="curvedBottom">
                     <div className="whitebox">
                         <image style={{backgroundImage: `url(${girlsClothes})`}}></image>
                         <div className="informationWrapper">
@@ -235,27 +221,69 @@ export const HomePage = () => {
                             </p>
                         </div>
                     </div>
-                    
                 </sectioncurvedbottom>
             </div>
 
 
-            <section className="secOverflowHidden">
-                <image className="redBg" style={{backgroundImage: `url(${wedding})`}}></image>
-                <p className="redBgParahraph"> 
-                    <h2><span>WHEN</span> IT'S PERFORMED</h2>
-                    Chyabrung is performed during Limbu festivals, wedding, events, gathering and more...
-                </p>
-                <button className="madeButton" onClick={() => navigate("/WhenPerformed")}>Show More</button>
+            <section className="chyabrungMoreInfo">
+                <div className="chyabrungInfoContainer lightGreen">
+                    <image className="mediumGreen" style={{backgroundImage: `url(${Jhympta})`}}></image>
+                    <div className="flexColumnn">
+                        <div className="informationWrapper">
+                            <h2><span>OTHER</span> INSTRUMENTS</h2>
+                            <line className="darkGreen"></line>
+                            <p> 
+                                Chyabrung is not only the instrument played during a kelange performace. The females dancers also carries different percussion instruments and plays together with the chyabrung.
+                            </p>
+                        </div>
+                        <button className="madeButton" onClick={() => navigate("/Instruments")}>Show More</button>
+                    </div>
+                </div>
+                
+                <div className="chyabrungInfoContainer lightPurple">
+                    <div className="flexColumnn">
+                        <div className="informationWrapper">  
+                            <h2>HOW IT'S <span>MADE</span></h2>
+                            <line className="mediumPurple"></line>
+                            <p>
+                                Chyabrung is made of many diffferent materials which are found in the nature. Main materials are wood and different types of leathers.
+                            </p>
+                        </div>
+                        <button className="madeButton" onClick={() => navigate("/HowItsMade")}>Show More</button>
+                    </div>
+                    <image className="darkPurple" style={{backgroundImage: `url(${chyabrungMade})`}}></image>
+                </div>
             </section>
 
-            <section className="secOverflowHidden">
-                <image className="redBg" style={{backgroundImage: `url(${blessing})`}}></image>
-                <p className="redBgParahraph"> 
-                    <h2>WHY IT'S <span>IMPORTANT</span></h2>
-                    Chyabrung is not just a dance performance for entertainment... its blessing.
-                </p>
-                <button className="madeButton" onClick={() => navigate("/WhyImportant")}>Show More</button>
+
+            <section className="chyabrungMoreInfo">
+                <div className="chyabrungInfoContainer lightRed">
+                    <image className="darkRed" style={{backgroundImage: `url(${wedding})`}}></image>
+                    <div className="flexColumnn">
+                        <div className="informationWrapper">
+                            <h2><span>WHEN</span> IT'S PERFORMED</h2>
+                            <line className="darkRed"></line>
+                            <p> 
+                                Chyabrung is performed during many occasions and festivals like chasok tangnam, sesikpa tangnam, wedding, events, gathering and more...
+                            </p>
+                        </div>
+                        <button className="madeButton" onClick={() => navigate("/Instruments")}>Show More</button>
+                    </div>
+                </div>
+                
+                <div className="chyabrungInfoContainer lightYellow">
+                    <div className="flexColumnn">
+                        <div className="informationWrapper">  
+                            <h2>WHY IT'S <span>IMPORTANT</span></h2>
+                            <line className="darkYellow"></line>
+                            <p>
+                                Chyabrung is not just a dance performance for entertainment... it is deeply connect with Limbu people history and origin since the beginning of time.                            
+                            </p>
+                        </div>
+                        <button className="madeButton" onClick={() => navigate("/HowItsMade")}>Show More</button>
+                    </div>
+                    <image className="darkYellow" style={{backgroundImage: `url(${blessing})`}}></image>
+                </div>
             </section>
 
 {/*             
